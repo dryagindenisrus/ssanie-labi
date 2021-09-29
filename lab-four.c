5ed8c6cdec90b2d24d0c2df3cd88e7872bd3269b
danzo0l <dryagindenisrus@gmail.com>
2021-09-29 00:13:02 -0400

    four-lab

--- lab-four.c
+++ lab-four.c
@@ -7,30 +7,72 @@
 
 int main() {
     int g = 1000;
-    int j, i, count, n = 0;
-    int arr[g][g];
-    char str_input[g], str_words[g][g];       	
-    fgets(str_input, 1000, stdin);
-    
+    int j, i, count = 0, n = 0;
+    int arr[g][2];
 
-    for (i=0; i<g; i++) {
-        count = 0;
-        for (j=0; j<g; j++) {
-            if (str_input[j + n] == ' ') {
-                arr[i][0] = i;
-                arr[i][1] = count;
-                n += count;
-                count = 0;
-                break;
-            } else {
-                str_words[i][j] = str_input[j];
-                count ++;
+    int lenght, index;
+  
+  	short str_words[g][g];       	
+    
+  	char c;
+  
+  	short wordInd, symbInd;
+  	wordInd = 0;
+  	symbInd = 1;
+  
+    while ((c = getchar()) != '\n') {
+    	if (c == ' ') {
+        wordInd++;
+        str_words[wordInd][symbInd++] = '\0';
+        symbInd = 1;
+        continue;
+    }
+        str_words[wordInd][symbInd++] = c;
+      	str_words[wordInd][0]++;
+      	
+    }
+
+    int size = 0;
+    
+  	for (i = 0; i <= wordInd; i++) {
+        char ch = '\0';
+        int j = 0;
+        arr[i][0] = i;
+        size ++;
+        do {
+            ch = (char)str_words[i][j++];
+            arr[i][1]++;
+        } while (ch != '\0');
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = (size - 1); j > i; j--) {
+            if (arr[j - 1][1] < arr[j][1]) {
+                int temp0, temp1;
+                temp0 = arr[j - 1][0];
+                temp1 = arr[j - 1][1];
+                arr[j - 1][0] = arr[j][0];
+                arr[j - 1][1] = arr[j][1];
+                arr[j][0] = temp0;
+                arr[j][1] = temp1;
             }
         }
     }
 
-    for (i = 0; i < 10; i++) {
-        printf("%d, %d\n", arr[i][0], arr[i][1]);
+    for (i = 0; i <= size-1; i++) {
+        char ch = '\0';
+        int j = 0;
+        do {
+            int mog = arr[i][0];
+            ch = (char)str_words[mog][j++];
+            printf("%c", ch);
+        } while (ch != '\0');
+        printf(" ");
     }
+    printf("\n");
+
     return 0;
 }
+
+
+    
