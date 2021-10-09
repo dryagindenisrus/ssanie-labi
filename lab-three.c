8a3c4505d7cc1574c19779d75ea581de94859643
danzo0l <dryagindenisrus@gmail.com>
2021-10-09 11:34:32 -0400

    lab-three remake

--- lab-three.c
+++ lab-three.c
@@ -18,14 +18,15 @@ int testing() {
         {{9,56,124,675,345,647,234,575,233,657}, {790, 675}},
         {{456,436,787,523,634,340,123,213,433,54}, {56, 54}},
         {{1,4,5,23,67,34,123,23,43,213}, {3, 4}},
-    };
 
+    };
+    int len_arr = sizeof input_data[0][1][1] * 5 / 2 ;
     int ggg = sizeof input_data / 80;
     for (int i = 0; i < ggg; i++) {
         char status;
         status = 'F';
         int res;
-        res = blizh(input_data[i][0], input_data[i][1][0], 10);
+        res = blizh(input_data[i][0], input_data[i][1][0], len_arr);
         if (input_data[i][1][1] == res) { 
             status = 'T';
         }
