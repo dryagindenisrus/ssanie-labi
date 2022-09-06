f54412d124dcaad410f47dd76a0f005733245a4f
danzo0l <danzo0l.in@gmail.com>
2022-09-06 04:00:12 -0700

    17_lab fix bag - error: 'List<T>::Node' is not a template

--- 17/List.cpp
+++ 17/List.cpp
@@ -1,15 +1,8 @@
 #include "List.h"
 
-template <typename T>
-void List<T>::push_back(T data) {
-    if (head == nullptr) {
-        head = new Node<T>(data);
-    } else {
-        Node<T> *current = this -> head;
-        while (current->pNext != nullptr) {
-            current = current->pNext;
-        }
-        current->pNext = new Node<T>(data);
-    }
-    Size++;
-};
+
+
+
+// template <typename T>
+// List<T>::~List() {
+// };
--- 17/List.h
+++ 17/List.h
@@ -1,59 +1,77 @@
 #ifndef LIST_H
 #define LIST_H
 
-// template<typename T> class List {
-//     private:
-//         template<typename T> class Node {
-//             friend class List;
-//             public:
-//                 Node *pNext;
-//                 T data;
-//                 Node(T data=T(), Node *pNext=nullptr) {
-//                     this -> data = data;
-//                     this -> pNext = pNext;
-//                 };
-//         };  
-//     public:
-//         int getSize() {return Size;};
-//         List(/* args */);
-//         ~List();
-//         int Size;
-//         Node<T> *head;
-//         void push_back(T data);
-// };
-
-// template <typename T>
-// List<T>::List(/* args */) {
-//     this -> Size = 0;
-//     this -> head = nullptr;
-// }
-
-// template <typename T>
-// List<T>::~List() {
-// }
-
-
-
-template<typename T> class Node {
-    public:
-        Node *pNext;
-        T data;
-        Node(T data = T(), Node *pNext = nullptr) {
-            this->data = data;
-            this->nNext = pNext;
-        }
-};
-
-
-template<typename T> class List {
+template <typename T> 
+class List {
     private:
-        Node<T> *head;
-        int Size;
+        // template <typename T> 
+        class Node {
+            friend class List;
+            public:
+                Node *pNext;
+                T data;
+                Node(T data=T(), Node *pNext=nullptr) {
+                    this -> data = data;
+                    this -> pNext = pNext;
+                };
+        };  
     public:
-        List(/* args */);
+        List();
         ~List();
+        int Size;
+        List<T>::Node *head;
         void push_back(T data);
         int getSize() {return Size;};
 };
+
+template <typename T>
+List<T>::List() {
+    this -> Size = 0;
+    this -> head = nullptr;
+}
+
+template <typename T>
+List<T>::~List() {
+    
+}
+
+
+template <typename T>
+void List<T>::push_back(T data) {
+    if (head == nullptr) {
+        head = new List<T>::Node(data);
+    } else {
+        List<T>::Node *current = this -> head;
+        while (current->pNext != nullptr) {
+            current = current->pNext;
+        }
+        current->pNext = new List<T>::Node(data);
+    }
+    Size++;
+};
+
+
+
+// template<typename T> class Node {
+//     public:
+//         Node *pNext;
+//         T data;
+//         Node(T data = T(), Node *pNext = nullptr) {
+//             this->data = data;
+//             this->nNext = pNext;
+//         }
+// };
+
+
+// template<typename T> class List {
+//     private:
+//         Node<T> *head;
+//         int Size;
+//     public:
+//         List(/* args */);
+//         ~List();
+//         void push_back(T data);
+//         int getSize() {return Size;};
+// };
 
 #endif
