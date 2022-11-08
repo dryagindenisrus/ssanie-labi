f4a32df3b5b3656afee77fef98f16b9108d74b9d
Denis Dryagin <danzo0l.in@gmail.com>
2022-11-08 12:01:00 +0300

    kr_1

--- kr_1/labirint.cpp
+++ kr_1/labirint.cpp
@@ -1,56 +1,35 @@
 #include <iostream>
 
-int fx;
-int fy;
+int x, y, fx, fy;
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {-1, 0, 1, 0};
-const int size = 4;
+const int size = 5;
 int count = 0;
 
-void print(std::ostream &os, int a[][size]) {
-    for (int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
-            std::cout << (a[i][j]) << " ";
-        }
-        std::cout << std::endl << std::endl;
-    }
-}
-
 void foo(int n, int a[][size], int x, int y){
-    print(std::cout, a);
-    std::cout << x << " ++++ " << y << std::endl;
-    // Это активировать если нужно чтобы остановился на первом правильном пути
-    // if (count == 1) return;
-
-    // Добавляем то, что мы тут прошли
+    if (count == 1) return;
     a[x][y] = n;
-
-    // Работаем по направлениям
+    std::cout << "(" << x << ", " << y << "), ";
     for (int i = 0; i < 4; i++){
         int nextX = x + dx[i], nextY = y + dy[i];
         if (( nextX >= size) || (nextX < 0)) continue;
         if (( nextY >= size) || (nextY + dy[i] < 0)) continue;
         if (a[nextX][nextY] == -3) {
-            print(std::cout, a);
-            std::cout << x << " --------- " << y << std::endl;
             count++;
         }
         if (a[nextX][nextY] != 0) continue;
         foo(n+1, a, x + dx[i], y + dy[i]);
     }
-
     a[x][y] = 0;
 }
 
 int main() {
-    int data[size][size] = { {0, 0, 0, 0},
-                             {0, -1, -3, 0},
-                             {0, -1, -1, 0},
-                             {-2, 0, 0, 0} };
-
-    // Наши координаты
-    int x, y;
-
+    int data[size][size] = {
+        {0,  0,  0,  0, 0}, 
+        {0, -1, -3, -1, 0}, 
+        {0, -1, -1,  0, 0}, 
+        {0,  0,  0,  0, 0}, 
+        {-2, 0,  -1, 0, 0}};
     x = 3, y = 0;
     fx = 1, fy = 2;
 
