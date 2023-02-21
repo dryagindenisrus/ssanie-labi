82c4e989d1cab198956bc4ac7107078c2591ebb9
danzo0l <danzo0l.in@gmail.com>
2023-02-21 06:59:39 -0800

    2 lab completed

--- java/2/Main.java
+++ java/2/Main.java
@@ -40,6 +40,8 @@ public class Main {
       d = d.product(c);
     }
 
+    d.doSortRows();
+
     System.out.println("A ^ 10 = " + d);
   }
 }
--- java/2/Marix.java
+++ java/2/Marix.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Matrix {
   private int[][] matrix;
@@ -46,4 +47,20 @@ class Matrix {
   public String toString() {
     return Arrays.deepToString(matrix);
   }
+
+  void doSortRows() {
+    matrix = Arrays.sort(matrix, new Comparator<int[]>() {
+          @Override
+          public int compare(int[] a, int[] b) {
+              return Integer.compare(Arrays.stream(a).sum(), Arrays.stream(b).sum());
+          }
+      });
+  }
+
+//   Arrays.sort(twoDim, new Comparator<int[]>() {
+//     @Override
+//     public int compare(int[] o1, int[] o2) {
+//         return Integer.compare(o2[0], o1[0]);
+//     }
+// });
 }
