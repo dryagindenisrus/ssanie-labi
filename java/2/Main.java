9ae65b9c0961d8cc9bad83bdc51b8c78c8deeaaf
danzo0l <danzo0l.in@gmail.com>
2023-02-21 04:26:47 -0800

    for pull

--- java/2/Main.java
+++ java/2/Main.java
@@ -1,53 +1,3 @@
-import java.util.Arrays;
-
-class Matrix {
-  private int[][] matrix;
-
-  Matrix(int size) {
-    matrix = new int[size][size];
-    for (int i = 0; i < size; i++) {
-      matrix[i][i] = 1;
-    }
-  }
-
-  public Matrix sum(Matrix b) {
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < matrix.length; j++) {
-        matrix[i][j] += b.matrix[i][j];
-      }
-    }
-    return this;
-  }
-
-  public Matrix product(Matrix b) {
-    Matrix res = new Matrix(matrix.length);
-    for (int i = 0; i < res.matrix.length; i++) {
-      res.matrix[i][i] = 0;
-    }
-    for (int i = 0; i < matrix.length; i++) {
-      for (int j = 0; j < b.matrix.length; j++) {
-        for (int k = 0; k < b.matrix.length; k++) {
-          res.matrix[i][j] += matrix[i][k] * b.matrix[k][j];
-        }
-      }
-    }
-    return res;
-  }
-
-  public void setElement(int row, int column, int value) {
-    matrix[row][column] = value;
-  }
-
-  public int getElement(int row, int column) {
-    return matrix[row][column];
-  }
-
-  @Override
-  public String toString() {
-    return Arrays.deepToString(matrix);
-  }
-}
-
 public class Main {
   public static void main(String[] args) throws Exception {
     Matrix a = new Matrix(2);
@@ -61,6 +11,8 @@ public class Main {
     b.setElement(0, 1, 6);
     b.setElement(1, 0, 5);
     b.setElement(1, 1, 2);
+
+    
 
     // matrix a
     System.out.println("A = " + a.toString());
