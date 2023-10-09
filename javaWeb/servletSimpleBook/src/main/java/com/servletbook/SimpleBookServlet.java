e6ebc32488f388c525feb5284a622f6781ec165e
Denis Dryagin <danzo0l.in@gmail.com>
2023-10-09 22:02:17 +0300

    servletSimpleBook: add gradle configs

---
+++ javaWeb/servletSimpleBook/src/main/java/com/servletbook/SimpleBookServlet.java
@@ -0,0 +1,27 @@
+package com.servletbook;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/my-servlet")
+public class SimpleBookServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        try (PrintWriter writer = response.getWriter();
+             BufferedReader reader = new BufferedReader(new FileReader(getServletContext().getRealPath("/index.html")))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.println(line);
+            }
+        }
+    }
+}
