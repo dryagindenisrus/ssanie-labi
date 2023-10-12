187fdaf33f9a6bc3df9c1827f3c6ef3d8b825f30
Denis Dryagin <danzo0l.in@gmail.com>
2023-10-12 09:00:23 +0300

    servletSimpleBook: servlet working

--- javaWeb/servletSimpleBook/src/main/java/com/servletbook/SimpleBookServlet.java
+++ javaWeb/servletSimpleBook/src/main/java/com/servletbook/SimpleBookServlet.java
@@ -3,12 +3,14 @@ package com.servletbook;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+@WebServlet("")
 public class SimpleBookServlet extends HttpServlet {
 
     private Notebook notebook;
